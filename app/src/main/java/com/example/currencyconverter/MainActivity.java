4b01be0f85599d1b0c6ca1497033eb24a5b6a26e
sojib <sojibahamed.just@gmail.com>
2019-03-03 04:28:59 +0600

    layout2

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] Currency;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Currency = getResources().getStringArray(R.array.currency);
+        spinner = findViewById(R.id.spinnerId);
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<>(this, R.layout.sample_view, R.id.textViewsampleId, Currency);
+        spinner.setAdapter(adapter);
     }
 }
